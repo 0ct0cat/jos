fb952b661cd40ac7cb8fe4a40063c6b30353dddb
Ryan Li <ryan@ryanium.com>
2011-04-26 17:31:03 +0800

    Set up _pgfault_upcall in pfentry.S and set_pgfault_handler in pgfault.c.

--- lib/pgfault.c
+++ lib/pgfault.c
@@ -28,8 +28,8 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 
 	if (_pgfault_handler == 0) {
 		// First time through!
-		// LAB 4: Your code here.
-		panic("set_pgfault_handler not implemented");
+		sys_page_alloc(env->env_id, (void *) UXSTACKTOP-PGSIZE, PTE_W);
+		sys_env_set_pgfault_upcall(env->env_id, _pgfault_upcall);
 	}
 
 	// Save handler pointer for assembly to call.
