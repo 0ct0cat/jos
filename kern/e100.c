da740549ea7a4d9c5cccdb0457122d5ca45ecc56
Ryan Li <ryan@ryanium.com>
2011-05-27 00:53:07 +0800

    Basic attach function for e100.

--- kern/e100.c
+++ kern/e100.c
@@ -1,1 +1,20 @@
-// LAB 6: Your driver code here
+#include <inc/x86.h>
+
+#include <kern/pci.h>
+#include <kern/e100.h>
+#include <kern/console.h>
+
+static uint8_t e100_irq;
+static uint32_t e100_base;
+
+int e100_attach(struct pci_func *f) {
+	pci_func_enable(f);
+
+	e100_irq = f->irq_line;
+	e100_base = f->reg_base[1];
+
+	// reset e100
+	outl(e100_base + 0x8, 0x0);
+
+	return 0;
+}
--- kern/e100.h
+++ kern/e100.h
@@ -1,5 +1,6 @@
 #ifndef JOS_KERN_E100_H
 #define JOS_KERN_E100_H
 
+int e100_attach(struct pci_func *f);
 
 #endif	// JOS_KERN_E100_H
