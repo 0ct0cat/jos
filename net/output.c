9a943a7712a4103b8295b29a8d6984cba7aeab38
Ryan Li <ryan@ryanium.com>
2011-05-29 14:35:23 +0800

    output.c: implement output helper environment for net.

--- net/output.c
+++ net/output.c
@@ -1,5 +1,7 @@
 #include "ns.h"
 
+#include <inc/syscall.h>
+
 extern union Nsipc nsipcbuf;
 
 void
@@ -7,7 +9,19 @@ output(envid_t ns_envid)
 {
 	binaryname = "ns_output";
 
-	// LAB 6: Your code here:
-	// 	- read a packet from the network server
-	//	- send the packet to the device driver
+	envid_t envid;
+
+	// read a packet from the network server
+	// send the packet to the device driver
+	while (1) {
+		ipc_recv(&envid, &nsipcbuf, 0);
+		if (envid != ns_envid)
+			continue;
+		unsigned int i;
+		cprintf("Data (%d): ", nsipcbuf.pkt.jp_len);
+		for (i = 0; i < nsipcbuf.pkt.jp_len; ++i)
+			cprintf("%x ", nsipcbuf.pkt.jp_data[i]);
+		cprintf("\n");
+		sys_transmit(nsipcbuf.pkt.jp_data, nsipcbuf.pkt.jp_len);
+	}
 }
